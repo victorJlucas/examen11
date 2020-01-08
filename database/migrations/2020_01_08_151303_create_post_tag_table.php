4d74f80239e3328a163ec61e9947422ad0819d04
Carlos Abrisqueta <cmabris@gmail.com>
2020-01-08 15:33:24 +0000

    Fin paso 7

---
+++ database/migrations/2020_01_08_151303_create_post_tag_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->on('posts')->references('id');
+
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->on('tags')->references('id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tag = new Tag;
+        $tag->name = 'Etiqueta 1';
+        $tag->save();
+
+        $tag->posts()->attach([1,2,3]);
+
+        $tag = new Tag;
+        $tag->name = 'Etiqueta 2';
+        $tag->save();
+
+        $tag->posts()->attach([2,3,4]);
+
+
+    }
+}
