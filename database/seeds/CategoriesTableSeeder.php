d9b54dd0fb7273da670d882ca66e6c9212ead849
Carlos Abrisqueta <cmabris@gmail.com>
2020-01-08 15:04:01 +0000

    Fin paso 6

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category = new Category();
+        $category->name = 'Categoría 1';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Categoría 2';
+        $category->save();
+    }
+}
