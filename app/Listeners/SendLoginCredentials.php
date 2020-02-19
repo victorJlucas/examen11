4bfd365a926e2a80c636d6008b267d2b4507e448
Carlos Abrisqueta <cmabris@gmail.com>
2020-02-19 15:39:07 +0000

    Paso 64

--- app/Listeners/SendLoginCredentials.php
+++ app/Listeners/SendLoginCredentials.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\UserWasCreated;
+use App\Mail\LoginCredentials;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
 
 class SendLoginCredentials
 {
@@ -26,6 +28,8 @@ class SendLoginCredentials
      */
     public function handle(UserWasCreated $event)
     {
-        //
+        Mail::to($event->user)->queue(
+            new LoginCredentials($event->user, $event->password)
+        );
     }
 }
