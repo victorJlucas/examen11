1371ab0ccfa1ade5f0af48e7fa18ff8d1d0779e4
Carlos Abrisqueta <cmabris@gmail.com>
2020-02-20 15:08:44 +0000

    Paso 68

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,83 @@
+@extends('admin.layouts.layout')
+
+@section('header')
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Roles <small> Listado</small></h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                <li class="breadcrumb-item active">Roles</li>
+            </ol>
+        </div><!-- /.col -->
+    </div>
+@endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Listado de roles</h3>
+            <a href="{{ route('admin.roles.create') }}" class="btn btn-primary float-right">
+                <i class="fa fa-plus">Crear Rol</i>
+            </a>
+        </div>
+        <div class="card-body">
+            <table id="roles-table" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Guard</th>
+                    <th>Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($roles as $role)
+                    <tr>
+                        <td>{{ $role->id }}</td>
+                        <td>{{ $role->name }}</td>
+                        <td>{{ $role->guard }}</td>
+                        <td>
+                            <a href="{{ route('admin.roles.show', $role) }}" class="btn btn-xs btn-default"><i class="fa fa-eye"></i></a>
+                            <a href="{{ route('admin.roles.edit', $role) }}" class="btn btn-xs btn-info"><i class="fa fa-pencil-alt"></i></a>
+                            <form action="{{ route('admin.roles.destroy', $role) }}" method="post" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-xs btn-danger" onclick="return confirm('¿Seguro que quieres eliminar este rol?')">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
+
+@push('styles')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+@endpush
+
+@push('scripts')
+    <!-- DataTables -->
+    <script src="/adminlte/plugins/datatables/jquery.dataTables.js"></script>
+    <script src="/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+
+    <script>
+        $(function () {
+            $('#roles-table').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+            });
+        });
+    </script>
+@endpush
+
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::group([
 
     Route::resource('users', 'UsersController', ['as' => 'admin']);
 
+    Route::resource('roles', 'RolesController', ['as' => 'admin']);
+
     Route::middleware('role:Admin')
         ->put('users/{user}/roles', 'UsersRolesController@update')
         ->name('admin.users.roles.update');
